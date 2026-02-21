2e32dfa2c5211df3a8f5378842a26633fffad31a
Namgit config --global user.email <divysaxena2402@gmail.com>
2026-02-21 23:40:38 +0530

    added otp featur

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
-  window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.custom-navbar-dark');
-    if (window.scrollY > 10) {
-      navbar.classList.add('shadow-lg');
-    } else {
-      navbar.classList.remove('shadow-lg');
-    }
-  });
-
-  
-<script>
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('scroll', () => {
   const navbar = document.querySelector('.custom-navbar-dark');
-  
-  window.addEventListener('scroll', function() {
+  if (window.scrollY > 10) {
+    navbar.classList.add('shadow-lg');
+  } else {
+    navbar.classList.remove('shadow-lg');
+  }
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navbar = document.querySelector('.custom-navbar-dark');
+
+  window.addEventListener('scroll', function () {
     if (window.scrollY > 50) {
       navbar.classList.add('scrolled');
     } else {
@@ -21,4 +21,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
-</script>
+</script >
